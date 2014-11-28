aca6bc93a63c6d6a84efb0a2d8c39cd1310f532e
baonguyen1994 <toninguyen10@gmail.com>
2014-11-28 11:32:32 +0700

    No Action Button Clear #11

--- src/jWhiteBoard/JWhiteBoard.java
+++ src/jWhiteBoard/JWhiteBoard.java
@@ -439,7 +439,7 @@ public class JWhiteBoard extends ReceiverAdapter implements ActionListener, Chan
      */
     public void actionPerformed(ActionEvent e) {
         String     command=e.getActionCommand();
-        if("Clear".equals(command)) {
+        if(e.getSource()==clearButton) {//fix
             if(noChannel) {
                 clearPanel();
                 return;
